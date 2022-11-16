7f8f7f79eaed98690f2fd11ed5fcb240a1012eda
“liambrockpy” <liambrock72@gmail.com>
2022-11-16 11:17:04 +0000

    Initialise service functions

---
+++ server/controllers/search/search.service.js
@@ -0,0 +1,13 @@
+const results = require('../../data')
+
+const findAllByQuery = async (query) => new Promise((res, rej) => {
+    res()
+})
+const findRandomByQuery = async (query) => new Promise((res, rej) => {
+    res()
+})
+
+module.exports = {
+    findAllByQuery,
+    findRandomByQuery
+}
