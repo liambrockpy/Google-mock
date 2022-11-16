8b80ab7521d66261800d743a90098e1682cbe734
“liambrockpy” <liambrock72@gmail.com>
2022-11-16 11:16:01 +0000

    Outline tests needed

--- server/tests/app.spec.js
+++ server/tests/app.spec.js
@@ -1,13 +1,19 @@
 const request = require('supertest')
 const app = require('../app')
 
-describe(`GET /`, () => {
-    it('should respond 200 with json', async () => {
-        request(app).get('/search')
-            .expect(200)
-            .then(res => {
-                expect(res.headers['content-type']).toMatch(/json/)
-                expect(res.body).toEqual({ testMessage: 'Hello, World!' })
-            })
-    })
+describe(`GET / with query`, () => {
+    // it('should respond 200 with json', async () => {
+    //     request(app).get('/search')
+    //         .expect(200)
+    //         .then(res => {
+    //             expect(res.headers['content-type']).toMatch(/json/)
+    //             expect(res.body).toEqual({ testMessage: 'Hello, World!' })
+    //         })
+    // })
+
+    it.todo('should respond 200 with all search results related to query')
+})
+
+describe(`GET /random with query`, () => {
+    it.todo('should respond 200 with a random search result related to query')
 })
