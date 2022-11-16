7e1f36e1e892533855580d42205781e2b80000f6
“liambrockpy” <liambrock72@gmail.com>
2022-11-16 11:16:31 +0000

    Create routes to service functions

--- server/controllers/search/search.router.js
+++ server/controllers/search/search.router.js
@@ -2,8 +2,30 @@ const express = require('express')
 const searchService = require('./search.service')
 const searchRouter = express.Router()
 
+// test route
+// searchRouter.get('/', async (req, res) => {
+//     try {
+//         res.status(200).json({ testMessage: 'Hello, World!' })
+//     } catch (err) {
+//         res.status(500).send(err.message)
+//     }
+// })
+
+// search query route
 searchRouter.get('/', async (req, res) => {
+    const searchQuery = req.query
     try {
+        const allResults = searchService.findAllByQuery(searchQuery)
+        res.status(200).json({ testMessage: 'Hello, World!' })
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+})
+
+searchRouter.get('/random', async (req, res) => {
+    const searchQuery = req.query
+    try {
+        const randomResult = searchService.findRandomByQuery(searchQuery)
         res.status(200).json({ testMessage: 'Hello, World!' })
     } catch (err) {
         res.status(500).send(err.message)
