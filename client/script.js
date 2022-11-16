4e5cb7d68b1ddf69d4bdbc6bc15c4d0b025889a9
“liambrockpy” <liambrock72@gmail.com>
2022-11-16 17:07:33 +0000

    Add error handling

--- client/script.js
+++ client/script.js
@@ -1,30 +1,40 @@
 // searchBtn.addEventListener('click', getAllResults);
 // luckyBtn.addEventListener('click', getRandomResult)
 const myForm = document.querySelector("form");
-myForm.addEventListener("submit", getGoogleResults)
+//// myForm.addEventListener("click", getGoogleResults)
+const searchButton = document.querySelector('#sbutton')
+const luckyButton = document.querySelector('#lucky')
+
+searchButton.addEventListener('click', getGoogleResults)
 
 const resultList = document.getElementById('googleResults');
 resultList.innerHTML = "";
 
-function getGoogleResults(e){
+function getGoogleResults(e) {
     e.preventDefault();
     // let h1 = document.getElementById("marvelname");
     // let h2 = document.getElementById("director");
 
-    let usersearch =document.getElementById("search_bar").value;
-    let url = `http://localhost:3000/search?value=${usersearch}`;
+    let userSearch = document.getElementById("search").value;
+    if (!userSearch) return //ignore empty strings
+    let url = `http://localhost:3000/search?value=${userSearch}`;
     fetch(url)
-    .then((response) => response.json())
-    .catch(data => console.log(data))
-    .then(data => {
-        console.log(data);
-        // h1.textContent=data.name;
-        // h2.textContent=data.director;
-        // h3.textContent=data.phase;
-        // h4.textContent=data.cast;
-        // let img = "img/" + data.id + ".jpeg";
-        // h5.setAttribute("src", img);
-        // h5.setAttribute("src", `$(data.name).jpeg`);
-    })
+        .then((response) => response.json())
+        .catch(data => console.log(data))
+        .then(data => {
+            console.log(data);
+            if (data.error) {
+                // ! handle error messages from google api
+                resultList.textContent = data.error.message
+            } else {
+                // h1.textContent=data.name;
+                // h2.textContent=data.director;
+                // h3.textContent=data.phase;
+                // h4.textContent=data.cast;
+                // let img = "img/" + data.id + ".jpeg";
+                // h5.setAttribute("src", img);
+                // h5.setAttribute("src", `$(data.name).jpeg`);
+            }
+        })
 }
 
