7df03e32bd499dc6369e3794a914e6a0b98f2862
“liambrockpy” <liambrock72@gmail.com>
2022-11-16 12:24:37 +0000

    Use google search api to fetch results

--- server/controllers/search/search.service.js
+++ server/controllers/search/search.service.js
@@ -1,10 +1,27 @@
-const results = require('../../data')
+const apiKey = 'AIzaSyA2aNF_jfiHbKp8zEedSNweFkkIKKmvbyo'
+const searchEngineId = 'f0a01621fcd2642c2'
+const googleSearchApi = `https://www.googleapis.com/customsearch/v1?key=${apiKey}&cx=${searchEngineId}&q=`
 
-const findAllByQuery = async (query) => new Promise((res, rej) => {
-    res()
+const findAllByQuery = async (clientQuery) => new Promise(async (res, rej) => {
+    const results = await fetch(`${googleSearchApi}${clientQuery}`)
+        .then(res => res.json())
+        .catch(err => rej(err))
+        .then(data => data.items)
+
+    res(results)
 })
-const findRandomByQuery = async (query) => new Promise((res, rej) => {
-    res()
+
+const findRandomByQuery = async (clientQuery) => new Promise(async (res, rej) => {
+    const results = await fetch(`${googleSearchApi}${clientQuery}`)
+        .then(res => res.json())
+        .catch(err => rej(err))
+        .then(data => {
+            const resultsLength = data.items.length
+            const randomIndex = Math.floor(Math.random() * resultsLength)
+            return data.items[randomIndex]
+        })
+
+    res(results)
 })
 
 module.exports = {
