dc93a4471d067a23953053183753e1ada8bcc599
Mizna Uppal <miznauppal@gmail.com>
2022-11-17 12:57:16 +0000

    done random button on main page

--- client/mainpage.js
+++ client/mainpage.js
@@ -1,20 +1,20 @@
-const myForm = document.querySelector("form");
-myForm.addEventListener("submit", getLucky)
+// const myForm = document.querySelector("form");
+// myForm.addEventListener("submit", getLucky)
 
-// const fetchAsync = async (index) => {
-//     const dbFile = await fetch(`http://localhost:3000/random/${index}`)
-//     const googleData = await dbFile.json();
-//     console.log(googleData);
+// // const fetchAsync = async (index) => {
+// //     const dbFile = await fetch(`http://localhost:3000/random/${index}`)
+// //     const googleData = await dbFile.json();
+// //     console.log(googleData);
+// // }
+
+// function getLucky(e){
+//     e.preventDefault();
+//     const resultList = document.getElementById('lucky');
+//     let url = `http://localhost:3000/random`;
+//     fetch(url)
+//     .then((response) => response.json())
+//     .catch(data => console.log(data))
+//     .then(data => {
+//         console.log(data.breadcrumb)
+//     })
 // }
-
-function getLucky(e){
-    e.preventDefault();
-    const resultList = document.getElementById('lucky');
-    let url = `http://localhost:3000/random`;
-    fetch(url)
-    .then((response) => response.json())
-    .catch(data => console.log(data))
-    .then(data => {
-        console.log(data.breadcrumb)
-    })
-}
