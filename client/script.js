e35e358b18b4482baf0146a8b75c219f68997b6f
Mizna Uppal <miznauppal@gmail.com>
2022-11-17 12:26:31 +0000

    now

--- client/script.js
+++ client/script.js
@@ -8,6 +8,7 @@ const fetchAsync = async (index) => {
     console.log(googleData);
 }
 
+
 async function append(input) {
     let m;
     let n;
@@ -58,40 +59,64 @@ async function clear() {
     resultList.innerHTML = '';
 }
 
-const form = document.getElementById('searchform');
-const btn1 = document.getElementById('submitButton1');
-const btn2 = document.getElementById('submitButton2');
-let searchCount = 0;
-let randomCount = 0;
+//random button feature
+// const form = document.getElementById('searchform');
+// const btn1 = document.getElementById('submitButton1');
+// const btn2 = document.getElementById('submitButton2');
 
-btn1.addEventListener('click', () => searchCount++)
-btn2.addEventListener('click', () => randomCount++)
+// let searchCount = 0;
+// let randomCount = 0;
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    console.log(e.target.value);
-    const searchQuery = document.getElementById('search_bar').value;
-    // filters non-letters
-    const filter = /[^A-Za-z]/g;
-    const filteredQuery = searchQuery.replace(filter, '');
+// btn1.addEventListener('click', () => searchCount++)
+// btn2.addEventListener('click', () => randomCount++)
 
-    // error alert
-    const search = filteredQuery.toLowerCase()
-    const match = (search === 'canada') || (search === 'newzealand') || (search === 'unitedkingdom');
-    if (match && searchCount === 1) {
-        append(search)
-    } else if (match && randomCount === 1) {
-        randomFetch(search)
-    } else if (search === '' && randomCount === 1) {
-        randomWebsite()
-    } else {
-        alert(`Search query for ${searchQuery} does not exist yet. Only "canada", "newzealand", or "unitedkingdom" instead.`)
-    }
-    clear()
-    searchCount = 0
-    randomCount = 0
-})
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault()
+//     console.log(e.target.value);
+//     const userInput = document.getElementById('searchbar').value;
+    
+//     // filters non-letters
+//     const filter = /[^A-Za-z]/g;
+//     const filteredQuery = userInput.replace(filter, '');
+//     // error alert
+//     const search = filteredQuery.toLowerCase()
+//     const match = (search === 'canada') || (search === 'newzealand') || (search === 'unitedkingdom');
+   
+//     if (match && searchCount === 1) {
+//         append(search)
+//     } else if (match && randomCount === 1) {
+//         randomFetch(search)
+//     } else if (search === '' && randomCount === 1) {
+//         randomWebsite()
+//     } else {
+//         alert(`Search query for ${userInput} does not exist yet. Only "canada", "newzealand", or "unitedkingdom" instead.`)
+//     }
+//     clear()
+//     searchCount = 0
+//     randomCount = 0
+// })
 
 
 
-const btn = document.getElementById('submitButton1');
+// const btn = document.getElementById('submitButton1');
+// const btn22 = document.getElementById('submitButton2')
+
+// //random
+// const randomFetch = async (country) => {
+//     const randData = await fetch(`http://localhost:3000/random/${country}`)
+//     const randomData = await randData.json();
+//     console.log(randomData)
+
+//     window.location.href = randomData.url
+// }
+
+// async function randomWebsite(){
+//     let getAwebsite = await fetch(`http://localhost:3000/random`)
+//     let countryDataAdd= await getAwebsite.json();
+//     console.log(countryDataAdd);
+
+//     window.location.href = countryDataAdd.url
+// }
+
+// fetchAsync(index).catch(err => console.log(err));
+
